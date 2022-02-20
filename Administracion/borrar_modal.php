9a48aceb651943c1a54e0d01e27cf352baf3a7c5
Oscar Linares <oscar.alejandro.linares@gmail.com>
2022-02-20 16:27:54 -0600

    bug modal eliminar

--- Administracion/borrar_modal.php
+++ Administracion/borrar_modal.php
@@ -4,7 +4,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <button type="button" class="close" data-bs-dismiss="modal" aria-hidden="true">&times;</button>
                 <center><h4 class="modal-title" id="myModalLabel">Eliminar producto</h4></center>
             </div>
             <div class="modal-body">
